0153eaee36dbbd0d0424379748fb038671c30699
ulaecio <ulaecio@live.com>
2024-05-26 17:14:01 -0300

    Backend

---
+++ backend/src/main/java/com/ulasoftware/picpaychallege/PicpaychallegeApplication.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.picpaychallege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaychallegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaychallegeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ulasoftware/picpaychallege/PicpaychallegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.picpaychallege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaychallegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
